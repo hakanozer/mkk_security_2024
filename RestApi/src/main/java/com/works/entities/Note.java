7f4bf7413fcce73930f0be54060a092c22169476
Hakan ÖZER <hakanozer02@gmail.com>
2024-10-01 16:33:21 +0300

    RestApi

---
+++ RestApi/src/main/java/com/works/entities/Note.java
@@ -0,0 +1,21 @@
+package com.works.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer nid;
+
+    private String title;
+    private String detail;
+
+
+}
